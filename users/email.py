5bcfed54dbca9fd6812855902ca5621a0714540f
doom <phramov945@gmail.com>
2023-01-28 22:38:45 +0300

    Bugfix v1.3

--- users/email.py
+++ users/email.py
@@ -11,7 +11,7 @@ class PasswordResetEmail(BaseEmailMessage):
         context = super().get_context_data()
 
         user = context.get("user")
-        context["domain"] = '158.160.37.55:82'
+        context["domain"] = 'ads-online.ml'
         context["uid"] = utils.encode_uid(user.pk)
         context["token"] = default_token_generator.make_token(user)
         context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
