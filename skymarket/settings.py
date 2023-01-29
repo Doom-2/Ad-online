ba53d2b99db66135f189c1052a063793b0c490a9
doom <phramov945@gmail.com>
2023-01-29 22:06:36 +0300

    Replace DEFAULT_AUTHENTICATION_CLASSES

--- skymarket/settings.py
+++ skymarket/settings.py
@@ -91,7 +91,8 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
     ),
 }
 
